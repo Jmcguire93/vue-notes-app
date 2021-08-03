f1e60d8ee86247fffbe1c2163526650204ba4541
James <james.ryan.mcguire@gmail.com>
2021-08-03 15:22:39 -0700

    Links js data through components

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="note-selectors">
-    <NoteSelector v-for="note in notes" v-bind:note="note" :key="note.id" />
-    <!-- <NoteSelector />
-    <NoteSelector /> -->
+    <NoteSelector
+      v-for="note in notes"
+      v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
+      v-on:selectNote="selectNote"
+      :key="note.id"
+    />
   </div>
 </template>
 
 <script>
 import NoteSelector from "./NoteSelector.vue";
-
 export default {
   components: {
     NoteSelector,
@@ -21,7 +24,13 @@ export default {
         { id: 3, body: "this is my third note", timestamp: Date.now() },
         { id: 4, body: "this is my fourth note", timestamp: Date.now() },
       ],
+      selectedNoteId: 1,
     };
+  },
+  methods: {
+    selectNote: function (note) {
+      this.selectedNoteId = note.id;
+    },
   },
 };
 </script>
