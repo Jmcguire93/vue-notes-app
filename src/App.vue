076151113d5a00802b9e3e8c2c51cbcddd28832f
James <james.ryan.mcguire@gmail.com>
2021-08-03 14:43:14 -0700

    Creates NoteSelectors and NoteSelector components

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,28 @@
   <div id="app">
     <Toolbar />
     <NoteContainer />
-    <!-- <div class="note-container">
-      <div class="note-selectors">
-        <div class="note-selector active">
-          <p class="note-selector-title">First note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-        <div class="note-selector">
-          <p class="note-selector-title">Second note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-        <div class="note-selector">
-          <p class="note-selector-title">Third note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-      </div>
-      <div class="note-editor">
-        <p class="note-editor-info">Timestamp here...</p>
-        <textarea class="note-editor-input">
-	          First note...
-
-	          Note text here...
-	        </textarea
-        >
-      </div>
-    </div> -->
+    <!-- <NoteSelectors /> -->
+    <!-- <NoteSelector /> -->
   </div>
 </template>
 
 <script>
 import Toolbar from "./components/Toolbar.vue";
 import NoteContainer from "./components/NoteContainer.vue";
+// import NoteSelectors from "./components/NoteSelectors.vue";
+// import NoteSelector from "./components/NoteSelector.vue";
 
 export default {
   name: "app",
   components: {
     Toolbar,
     NoteContainer,
+    // NoteSelectors,
+    // NoteSelector,
   },
 };
 </script>
+s
 
 <style>
 /* RESET */
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="note-selectors">
+    <NoteSelector v-for="note in notes" :key="note.id" />
+    <!-- <NoteSelector />
+    <NoteSelector /> -->
+  </div>
+</template>
+
+<script>
+import NoteSelector from "./NoteSelector.vue";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "this is my first note", timestamp: Date.now() },
+        { id: 2, body: "this is my second note", timestamp: Date.now() },
+        { id: 3, body: "this is my third note", timestamp: Date.now() },
+      ],
+    };
+  },
+};
+</script>
